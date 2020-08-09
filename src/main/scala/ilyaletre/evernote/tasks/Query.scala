4c8001bfa6dba4df15817203aa1992ae7d8ae849
utky <ilyaletre@gmail.com>
2020-08-09 23:54:02 +0000

    test for timespan

--- src/main/scala/ilyaletre/evernote/tasks/Query.scala
+++ src/main/scala/ilyaletre/evernote/tasks/Query.scala
@@ -1,11 +1,26 @@
 package ilyaletre.evernote.tasks 
 
+import java.time.{Instant, ZoneId}
+import java.time.format.{DateTimeFormatter}
+
+sealed trait TimeSpan
+case class DaysBefore(before: Int) extends TimeSpan {
+  override def toString(): String = s"day-$before"
+}
+case class At(at: Instant) extends TimeSpan {
+  override def toString(): String = TimeSpan.timeFormat.format(at)
+}
+
+object TimeSpan {
+  val timeFormat = DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmssX").withZone(ZoneId.of("UTC"))
+}
+
 // Search grammer
 // https://dev.evernote.com/doc/articles/searching_notes.php
 sealed trait Predicate
 case class Notebook(name: String) extends Predicate
 case class Title(name: String) extends Predicate
-//case class Created(name: String) extends Predicate
+case class Created(time: TimeSpan) extends Predicate
 case class Not(p: Predicate) extends Predicate
 
 object Predicate {
@@ -13,6 +28,7 @@ object Predicate {
     p match {
       case Notebook(name) => ("notebook", name)
       case Title(name) => ("intitle", s""""$name"""")
+      case Created(t) => ("created", t.toString())
       case Not(Not(p)) => Predicate.asPair(p)
       case Not(p) => {
         val (k, v) = Predicate.asPair(p)
--- src/test/scala/ilyaletre/evernote/tasks/QuerySpec.scala
+++ src/test/scala/ilyaletre/evernote/tasks/QuerySpec.scala
@@ -1,6 +1,7 @@
 package ilyaletre.evernote.tasks
 
 import org.scalatest.funspec.AnyFunSpec
+import java.time.{ZonedDateTime, LocalDate, LocalTime, ZoneId}
 
 class QuerySpec extends AnyFunSpec {
   describe("Query") {
@@ -8,12 +9,33 @@ class QuerySpec extends AnyFunSpec {
       assert(Query.fromPredicates(Seq(Notebook("test")))
         == "notebook:test")
     }
+    it("should convert negative predicate to string") {
+      assert(Query.fromPredicates(Seq(Not(Notebook("test"))))
+        == "-notebook:test")
+    }
     it("should convert predicates to string") {
       assert(Query.fromPredicates(
         Seq(Notebook("testnb"), Title("test title")))
         == "notebook:testnb intitle:\"test title\"")
     }
   }
-
+  describe("TimeSpan") {
+    it("convert time at") {
+      assert(DaysBefore(3).toString() == "day-3")
+    }
+    it("days ago") {
+      assert(At(java.time.Instant.ofEpochSecond(1597014960)).toString()
+        == "20200809T231600Z")
+    }
+    it("days ago with timezone") {
+      val dt = ZonedDateTime.of(
+        LocalDate.of(2020, 8, 10),
+        LocalTime.of(8, 16, 0),
+        ZoneId.of("Asia/Tokyo")
+      ).toInstant()
+      assert(At(dt).toString()
+        == "20200809T231600Z")
+    }
+  }
 }
 
