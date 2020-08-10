6272f0b7f388af29b3ea40a1f7b5426548082a74
utky <ilyaletre@gmail.com>
2020-08-10 23:19:07 +0000

    add doc for API method

--- src/main/scala/ilyaletre/evernote/tasks/Api.scala
+++ src/main/scala/ilyaletre/evernote/tasks/Api.scala
@@ -2,18 +2,24 @@ package ilyaletre.evernote.tasks
 
 import scala.util.{Try}
 
+/**
+  * Find template from notebook 'template' with specific title
+  */
 trait GetTemplate[T] {
   def getTemplate(self: T, title: String): Try[Template]
 }
 
+/**
+  * Create note with the title and content under the notebook with tags
+  */
 trait CreateNote[T] {
-  def createNote(self: T, content: String): Try[Unit]
+  def createNote(self: T, title: String, content: String, notebook: String, tags: Seq[String]): Try[Unit]
 }
 
 object Api {
   def getTemplate[T](implicit instance: GetTemplate[T], self: T, title: String): Try[Template]
     = instance.getTemplate(self, title)
 
-  def createNote[T](implicit instance: CreateNote[T], self: T, content: String): Try[Unit]
-    = instance.createNote(self, content)
+  def createNote[T](implicit instance: CreateNote[T], self: T, title: String, content: String, notebook: String, tags: Seq[String]): Try[Unit]
+    = instance.createNote(self, title, content, notebook, tags)
 }
--- src/main/scala/ilyaletre/evernote/tasks/Evernote.scala
+++ src/main/scala/ilyaletre/evernote/tasks/Evernote.scala
@@ -8,7 +8,40 @@ case class Evernote(noteStoreClient: NoteStoreClient)
 
 object Evernote {
   implicit val getTemplate = new GetTemplate[Evernote] {
+    /**
+      * 1. [[findNotesMetadata https://dev.evernote.com/doc/reference/javadoc/com/evernote/edam/notestore/NoteStore.Client.html#findNotesMetadata(java.lang.String,%20com.evernote.edam.notestore.NoteFilter,%20int,%20int,%20com.evernote.edam.notestore.NotesMetadataResultSpec)]] w/ filter notebook, intitle
+      * 2. [[NoteList https://dev.evernote.com/doc/reference/javadoc/com/evernote/edam/notestore/NoteList.html]] length should be 1
+      * 3. [[getNote https://dev.evernote.com/doc/reference/javadoc/com/evernote/edam/notestore/NoteStore.Client.html#getNote(java.lang.String,%20java.lang.String,%20boolean,%20boolean,%20boolean,%20boolean)]] by guid of first of 2. 
+      * @param self
+      * @param title
+      * @return
+      */
     def getTemplate(self: Evernote, title: String): Try[Template] = {
+      ???
+    }
+  }
+  implicit val createNote = new CreateNote[Evernote] {
+    /**
+      * 
+      *
+      * 1. [[listNotebooks https://dev.evernote.com/doc/reference/javadoc/com/evernote/edam/notestore/NoteStore.Client.html#listNotebooks(java.lang.String)]] and get matched one
+      * 2. instantiate Note
+      * 3. Set attributes
+      *     A) title
+      *     B) content XHTML
+      *     C) active
+      *     D) notebookGuid
+      *     C) tagNames
+      * 4. [[createNote https://dev.evernote.com/doc/reference/javadoc/com/evernote/edam/notestore/NoteStore.Client.html#createNote(java.lang.String,%20com.evernote.edam.type.Note)]]
+      * 
+      * @param self
+      * @param title
+      * @param content
+      * @param notebook
+      * @param tags
+      * @return
+      */
+    def createNote(self: Evernote, title: String, content: String, notebook: String, tags: Seq[String]): Try[Unit] = {
       ???
     }
   }
